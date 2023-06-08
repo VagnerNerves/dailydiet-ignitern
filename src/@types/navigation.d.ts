e7528a36c212972f1ab5dd6f0fa160d82efabd32
Vagner Nerves <vagnernervessantos@gmail.com>
2023-06-08 10:48:31 -0300

    feat: Created Typescript for Routes.

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,17 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      home: undefined
+      statistics: undefined
+      newmeal: {
+        id: string
+      }
+      newmealfeedback: {
+        isOnDiet: boolean
+      }
+      viewmeal: {
+        id: string
+      }
+    }
+  }
+}
