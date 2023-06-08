be6ea46f2b69bd0354729520b6bd6af94f1b699b
Vagner Nerves <vagnernervessantos@gmail.com>
2023-06-08 14:27:23 -0300

    feat: Ajusted for onPress this button and navigation.

--- src/components/HeaderNavigate/index.tsx
+++ src/components/HeaderNavigate/index.tsx
@@ -1,3 +1,5 @@
+import { useNavigation } from '@react-navigation/native'
+
 import { ArrowLeft } from 'phosphor-react-native'
 import { useTheme } from 'styled-components/native'
 import { ButtonBack, Container, Description, Title, TypeHeader } from './styles'
@@ -13,22 +15,25 @@ interface HeaderNavigateProps {
   description?: string
   type?: TypeHeader
   color?: 'default' | 'green' | 'red'
+  onPress?: () => void
 }
 
 export function HeaderNavigate({
   title,
   description,
   type = 'default',
-  color = 'default'
+  color = 'default',
+  onPress
 }: HeaderNavigateProps) {
   const { COLORS } = useTheme()
+  const navigation = useNavigation()
 
   return (
     <Container type={type}>
       <Title type={type}>{title}</Title>
       {description && <Description>{description}</Description>}
 
-      <ButtonBack>
+      <ButtonBack onPress={onPress ? onPress : () => navigation.goBack()}>
         <ArrowLeft size={24} color={COLORS[COLORS_ARROW[color]]} />
       </ButtonBack>
     </Container>
